10a4a2d7c1034595d84d26704d8ef536da0cafee
suixin <leon14214@icloud.com>
2015-04-22 00:31:29 +0800

    修改查找地板值算法的Bug

--- search.go
+++ search.go
@@ -1,25 +1,34 @@
 package goutils
 
+import (
+	"errors"
+	"fmt"
+)
+
 // 查找'地板值'
 // 也就是数组nums中最大的，小于num的值。
-// @param val 地板值
-// @param idx 其在数组中的索引
-func SearchFloor(num int64, nums []int64) (val, idx int64) {
+// @param val 地板值，如果没有找到则返回-1
+// @param idx 其在数组中的索引, 如果没有找到则返回-1
+// @param err 当没有找到时，一般是由于需要查找的值小于数组中的最小值，返回err
+func SearchFloor(num int64, nums []int64) (val, idx int64, err error) {
 	// TODO 先对nums排序
 	start := 0
 	end := len(nums) - 1
 	for {
 		mid := (start + end) / 2
+		fmt.Println("start: ", start, "; end: ", end, "; mid : ", mid)
 
 		if nums[mid] == num {
-			return nums[mid], int64(mid)
+			return nums[mid], int64(mid), nil
 		}
 
 		if end-start == 1 {
 			if num >= nums[end] {
-				return nums[end], int64(end)
+				return nums[end], int64(end), nil
+			} else if num >= nums[start] {
+				return nums[start], int64(start), nil
 			} else {
-				return nums[start], int64(start)
+				return -1, -1, errors.New("Not Found!")
 			}
 		}
 
--- search_test.go
+++ search_test.go
@@ -4,13 +4,24 @@ import "testing"
 
 func TestSearchFloor(t *testing.T) {
 	is := []int64{1, 3, 4, 6, 8, 11, 39, 210}
-	val, idx := SearchFloor(5, is)
-	if val != 4 || idx != 2 {
+	val, idx, err := SearchFloor(5, is)
+	if val != 4 || idx != 2 || err != nil {
 		t.Fatalf("Wanted: val = 4, idx = 2; Actual: val = %v, idx = %v", val, idx)
 	}
 
-	val, idx = SearchFloor(8, is)
+	val, idx, _ = SearchFloor(8, is)
 	if val != 8 || idx != 4 {
 		t.Fatalf("Wanted: val = 8, idx = 4; Actual: val = %v, idx = %v", val, idx)
+	}
+
+	val, idx, err = SearchFloor(0, is)
+	t.Logf("%v", err)
+	if val != -1 || idx != -1 || err == nil {
+		t.Fatalf("Wanted: val = -1, idx = -1; Actual: val = %v, idx = %v", val, idx)
+	}
+
+	val, idx, _ = SearchFloor(999, is)
+	if val != 210 || idx != 7 {
+		t.Fatalf("Wanted: val = 210, idx = 7; Actual: val = %v, idx = %v", val, idx)
 	}
 }
