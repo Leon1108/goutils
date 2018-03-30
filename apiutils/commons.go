41d49ac4335ff0e17c52ad01b731fac852e8f1b4
Sui Xin <suixin@creditx.com>
2018-03-30 12:37:11 +0800

    add api common

---
+++ apiutils/commons.go
@@ -0,0 +1,32 @@
+package apiutils
+
+import "fmt"
+
+type CommonStatus struct {
+	Code    CommonStatusCode `json:"code"`
+	Message string           `json:"message"`
+}
+
+func (cs *CommonStatus) String() string {
+	return fmt.Sprintf("Status { Code: %d; Message: %v }", cs.Code, cs.Message)
+}
+
+type CommonStatusCode int
+
+const (
+	CommonStatusCodeSuccess CommonStatusCode = 0
+	CommonStatusCodeFailed  CommonStatusCode = 1
+)
+
+type CommonResp struct {
+	Status  *CommonStatus `json:"status"`
+	Payload interface{}   `json:"payload,omitempty"`
+}
+
+func NewSuccessCommonResp(payload interface{}) *CommonResp {
+	return &CommonResp{&CommonStatus{CommonStatusCodeSuccess, "Success"}, payload}
+}
+
+func NewFailedCommonResp(err error) *CommonResp {
+	return &CommonResp{&CommonStatus{CommonStatusCodeFailed, err.Error()}, nil}
+}
